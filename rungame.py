860ea8a5fd7040733fe514a3d1d6ccf481402953
Anny Wei <annywei9@gmail.com>
2023-09-23 12:39:40 -0400

    added gitignore

---
+++ rungame.py
@@ -0,0 +1,7 @@
+import pygame as pg
+
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGHT = 1200
+
+gameDisplay = pg.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+pg.display.set_caption("A Dragon's Guide to Power")
