74ede7a92eaa49c044219fe33ef15e83d5b3599d
linazhuge <linazhuge18@gmail.com>
2023-09-23 19:35:25 -0400

    afd

--- rungame.py
+++ rungame.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from button import Button
 from datetime import datetime, timedelta
 from manipulate_health_data import wrkout_collection
 import time
@@ -32,27 +33,6 @@ def textObjects(text, font, colour):
     #returns text + rectangle
     return textSurface, textSurface.get_rect()
 
-#size -> fontsize
-def button(text, x, y, width, height, size, icolour, acolour, action = None):
-    #Detects user input from mouse
-    mouse = pg.mouse.get_pos()
-    click = pg.mouse.get_pressed()
-
-    #Checks position of mouse to be in the button area
-    for event in pg.event.get():
-        if x+width > mouse[0] > x and y+height > mouse[1] > y:
-            pg.draw.rect(gameDisplay, acolour,(x, y, width, height))
-            if event.type == pg.MOUSEBUTTONDOWN:
-                action() #activates action
-        else:
-            #Changes button colour
-            pg.draw.rect(gameDisplay, icolour,(x, y, width, height))
-
-    #Text of button
-    largeText = pg.font.Font('freesansbold.ttf', size)
-    textSurf, textRect = textObjects(text, largeText, BLACK)   
-    textRect.center = (x+(width/2), y+(height/2))
-    gameDisplay.blit(textSurf, textRect)
 
 def skillsPage():
     while True:
@@ -74,7 +54,7 @@ def newGoalPage():
         textSurf, textRect = textObjects("Add A New Goal: ", largeText, BRIGHT_GREEN)   
         textRect.center = (400, 300)
         gameDisplay.blit(textSurf, textRect)
-        button("Menu", 50, 400, 100, 75, 20, GREEN, BRIGHT_GREEN, menu )
+       # button("Menu", 50, 400, 100, 75, 20, GREEN, BRIGHT_GREEN, menu )
         pg.display.update()
         #clock.tick(60)
 
@@ -92,17 +72,15 @@ def menu():
     d = datetime(2023, 9, 1)
     gameRun = True
     start_time = datetime.now()
+
     while gameRun:
         print("menu")
+        PLAY_MOUSE_POS = pg.mouse.get_pos()
+
         if (datetime.now() - start_time).total_seconds() > 5:
             start_time = datetime.now()
             d += timedelta(days=1)
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                gameExit = False
-                quit()
         gameDisplay.fill(WHITE)
 
         dragonAnimation()
@@ -118,10 +96,29 @@ def menu():
        # displayProfile()
 
       #  displayMissions()
+        SKILLS = Button(image=None, pos=(200, 460), 
+                            text_input="SKILLZ", font=largeText, base_color=GREEN, hovering_color=BRIGHT_GREEN)
 
-        button("Skills", 50, 400, 100, 75, 20, GREEN, BRIGHT_GREEN, skillsPage )
-        button("+ Goal", 50, 500, 100, 75, 20, GREEN, BRIGHT_GREEN, newGoalPage)
+        SKILLS.changeColor(PLAY_MOUSE_POS)
+        SKILLS.update(gameDisplay)
 
+              #  displayMissions()
+        GOALS = Button(image=None, pos=(640, 460), 
+                            text_input="GOALS", font=largeText, base_color=GREEN, hovering_color=BRIGHT_GREEN)
+
+        GOALS.changeColor(PLAY_MOUSE_POS)
+        GOALS.update(gameDisplay)
+
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if SKILLS.checkForInput(PLAY_MOUSE_POS):
+                    skillsPage()
+                elif GOALS.checkForInput(PLAY_MOUSE_POS):
+                    newGoalPage()
         pg.display.update()
         #clock.tick(60)
 
